37f1229ce85beb4e25564f1717b4918eb1a57e6f
Andrzej Rapita <g4mba89@gmail.com>
2011-11-07 12:33:16 +0100

    fortunka

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, 'lUsFJ08E9XzpJdBQeEV3SA', 'pJTGCEcAK2ti6HmmDVlk4E4iw2WPNp6Pcfr5QmkIBk'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fortunki::Application.config.secret_token = 'f6362e12bdb1e75c04919fc36466f663ba24a3eabb8988b5eb96c7945bf6790ca3ca064ee693e974d0c8bff04c30ab37716736452127234aab94eab59948eee1'
